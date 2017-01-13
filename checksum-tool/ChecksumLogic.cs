eb4a0f4d0af88a247112be09cbab93b0c8b97d3b
Mark Wong <mark.sl.wong@gmail.com>
2017-01-13 03:51:31 -0800

    Initial commit

---
+++ checksum-tool/ChecksumLogic.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+
+namespace ChecksumTool
+{
+    public static class ChecksumLogic
+    {
+        public static void Run(DirectoryInfo targetDirectory, string outputFile, string outputNamespace, string outputClass)
+        {
+            var files = ListFilesRecusive(targetDirectory);
+            var hashes = new List<string>();
+
+            using (var algorithm = SHA256.Create())
+            {
+                foreach (var file in files)
+                {
+                    var bytes = File.ReadAllBytes(Path.Combine(targetDirectory.FullName, file));
+
+                    var hash = algorithm.ComputeHash(bytes);
+
+                    hashes.Add(Convert.ToBase64String(hash));
+                }
+            }
+
+            using (var fileStream = File.OpenWrite(outputFile))
+            using (var textWriter = new StreamWriter(fileStream))
+            {
+                textWriter.WriteLine("using System;");
+                textWriter.WriteLine("using System.Collections.Generic;");
+                textWriter.WriteLine();
+                textWriter.WriteLine();
+                textWriter.WriteLine("namespace {0}", outputNamespace);
+                textWriter.WriteLine("{");
+                textWriter.WriteLine("    public static class {0}", outputClass);
+                textWriter.WriteLine("    {");
+                textWriter.WriteLine("        public static readonly IDictionary<string, string> Hashes = new Dictionary<string, string>");
+                textWriter.WriteLine("        {");
+
+                for (var i = 0; i < files.Count(); ++i)
+                {
+                    var file = files[i];
+                    var hash = hashes[i];
+
+                    textWriter.WriteLine("            {{ @\"{0}\",", file);
+                    textWriter.WriteLine("                @\"{0}\" }},", hash);
+                }
+
+                textWriter.WriteLine("        };");
+                textWriter.WriteLine("    }");
+                textWriter.WriteLine("}");
+
+                textWriter.Close();
+                fileStream.Close();
+
+                Console.WriteLine("Checksum Complete");
+            }
+        }
+
+        private static IList<string> ListFilesRecusive(DirectoryInfo directory)
+        {
+            var results = new List<string>();
+
+            foreach (var file in directory.GetFiles())
+            {
+                results.Add(file.Name);
+            }
+
+            foreach (var subDirectory in directory.GetDirectories())
+            {
+                var subFiles = ListFilesRecusive(subDirectory);
+
+                foreach (var subFile in subFiles)
+                {
+                    results.Add(Path.Combine(subDirectory.Name, subFile));
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ checksum-tool/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace ChecksumTool
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Run(args);
+
+            Console.ReadKey();
+        }
+
+        private static void Run(string[] args)
+        {
+            if (args.Length != 4)
+            {
+                ArgumentError("Invalid number of arguments.");
+                return;
+            }
+
+            var targetDirectory = args[0];
+            var outputFile      = args[1];
+            var outputNamespace = args[2];
+            var outputClass     = args[3];
+
+            if (!Directory.Exists(targetDirectory))
+            {
+                ArgumentError("Target directory does not exist.");
+                return;
+            }
+
+            if (outputNamespace.Any(char.IsWhiteSpace))
+            {
+                ArgumentError("Output namespace cannot have any whitespace characters.");
+                return;
+            }
+
+            if (outputClass.Any(char.IsWhiteSpace))
+            {
+                ArgumentError("Output class cannot have any whitespace characters.");
+                return;
+            }
+            
+            var outputNamespaceSegments = outputNamespace.Split('.');
+
+            if (outputNamespaceSegments.Any(x => x.Length == 0 || !char.IsLetter(x[0]) || !x.All(char.IsLetterOrDigit)))
+            {
+                ArgumentError("Invalid namespace name (must start with letter, and only contain alpha-numeric characters.");
+                return;
+            }
+
+            if (outputClass.Length == 0 || !char.IsLetter(outputClass[1]) || !outputClass.All(char.IsLetterOrDigit))
+            {
+                ArgumentError("Invalid class name (must start with letter, and only contain alpha-numeric characters.");
+                return;
+            }
+            
+            try
+            {
+                ChecksumLogic.Run(new DirectoryInfo(targetDirectory), outputFile, outputNamespace, outputClass);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown error: " + e);
+            }
+        }
+
+        private static void ArgumentError(string description)
+        {
+            Console.WriteLine("ChecksumTool.exe [target-directory] [output-file] [output-namespace] [output-class]");
+            Console.WriteLine();
+            Console.WriteLine(description);
+        }
+    }
+}
